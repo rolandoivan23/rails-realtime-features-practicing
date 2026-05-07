b0e125e2e954b06f2d4ac561f20065c05c99a442
Rolando Vázquez <rolando.vazquez.23@gmail.com>
2026-05-06 20:27:42 -0600

    Fix NameError in presence reset initializer by wrapping in after_initialize

--- config/initializers/presence_reset.rb
+++ config/initializers/presence_reset.rb
@@ -1,2 +1,6 @@
 # Reset all users to offline on boot
-User.update_all(online: false) if ActiveRecord::Base.connection.table_exists?('users')
+Rails.application.config.after_initialize do
+  User.update_all(online: false) if ActiveRecord::Base.connection.table_exists?('users')
+rescue ActiveRecord::NoDatabaseError, ActiveRecord::StatementInvalid
+  # Skip if database/table doesn't exist yet (e.g. during migrations)
+end
