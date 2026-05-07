f085db5ee88759ed89c4c1e3e1a33331d046c350
Rolando Vázquez <rolando.vazquez.23@gmail.com>
2026-05-06 20:30:47 -0600

    Add belongs_to :user association to Message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :room
+  belongs_to :user
   validates :content, presence: true
 
   # This line is the "Rails Way" to trigger Action Cable broadcasts automatically.
